3d867da27434f99be3126049a0f9e8c71f564e1e
erdalaydin <erdalaydin92@hotmail.com>
2019-09-06 11:13:24 +0200

    Sequelize is done

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 4000;
+const db = require("./db");
 
 app.listen(`${port}`, () => console.log(` Server already started at ${port}`));
